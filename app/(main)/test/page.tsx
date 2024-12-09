1f59e1167a59a27c1488aac56fd6fd62bad40df0
fj <ku99.en+p@gmail.com>
2024-12-09 22:22:22 +0900

    feat: add prisma+supabase setting

--- app/(main)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TestPage = () => {
-  return (
-    <div>Hello This is a page?</div>
-  )
-}
-
-export default TestPage
---
+++ lib/initial-profile.ts
@@ -0,0 +1,37 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/db";
+import { use } from "react";
+
+
+
+export const initialProfile = async () => {
+    const { redirectToSignIn } = await auth();
+
+    const user = await currentUser();
+    if (!user) {
+        return redirectToSignIn();
+    }
+
+    const profile = await prisma.profile.findUnique({
+        where: {
+            userId: user.id,
+        }
+    });
+
+    if (profile) {
+        return profile;
+    }
+
+    const newProfile = await prisma.profile.create({
+        data: {
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    });
+
+    return newProfile;
+}
+
+
